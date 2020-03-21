2176f0c014633d6bcd2e844c30f2f759ae90a7ff
magdahime <magdalena.majkowska1@gmail.com>
2020-03-21 18:47:38 +0100

    Traverse function not working, problems with pathlib.Path() function

--- segmentation/driver.py
+++ segmentation/driver.py
@@ -9,5 +9,39 @@ from nyt_data import retrieve
 repoUrl = 'https://github.com/oreillymedia/doing_data_science/'
 fileUrl = 'raw/master/dds_datasets.zip'
 
-retrieve(repoUrl+fileUrl, 'raw_data')
+#retrieve(repoUrl+fileUrl, 'raw_data')
 print('Raw data files are successfully retrieved.')
+
+import numpy as np
+import pandas as pd
+from nyt_data import traverse
+
+summary_data = dict()
+summary_data.setdefault("CTR", np.empty(31))
+summary_data.setdefault("Clicks", np.empty(31))
+
+def select_stats_unregistered(df, file_num):
+    summary_data['CTR'][file_num] = df['CTR']['mean'][('Unknown', '0')]
+    summary_data["Clicks"][file_num] = df["Clicks"]["sum"][('Unknown', '0')]
+    
+
+traverse('./raw_data', select_stats_unregistered)
+
+print('Raw data files are successfully processed.')
+df = pd.DataFrame.from_dict(summary_data)
+import matplotlib.pyplot as plt
+
+fig, axes = plt.subplots(nrows=2, ncols=1)
+df['CTR'].plot(
+        title='Click Through Rate Over 1 Month',
+        ax=axes[0],
+        figsize=(8, 9),
+        xticks=[]
+);
+
+df['Clicks'].plot(
+        xticks=range(0, 31, 2),
+        title='Total Clicks Over 1 Month',
+        ax=axes[1],
+        figsize=(8, 9)
+);
--- segmentation/scripts/nyt_data.py
+++ segmentation/scripts/nyt_data.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import requests, zipfile, io, shutil
+import pandas as pd
+import numpy as np
+
 
 unpackedFolder = '/dds_datasets/'
 unpackedZipFile = 'dds_ch2_nyt.zip'
@@ -18,3 +21,44 @@ def retrieve(sourceFile, destinationFolder):
     z = zipfile.ZipFile(destinationFolder + unpackedFolder +unpackedZipFile)
     z.extractall(destinationFolder)
     cleanup()
+    
+def q25(x):
+   return x.quantile(0.25)
+
+def q75(x):
+    return x.quantile(0.75)
+    
+def summarize(data_file):
+    #Read and parse the CSV data file
+    nyt_data = pd.read_csv(data_file, dtype={'Gender':'category'})
+    # segment users into age groups
+    nyt_data["Age_group"] = pd.cut(nyt_data["Age"],
+                                   bins=[-1, 0, 17, 24, 34, 44, 54, 64, 120],
+                                   labels = ["Unknown",
+                                             "1-17",
+                                             "18-24",
+                                             "25-34",
+                                             "35-44",
+                                             "45-54",
+                                             "55-64",
+                                             "65+"])
+    nyt_data.drop("Age", axis='columns', inplace=True)
+    nyt_data["CTR"] = nyt_data["Clicks"]/nyt_data['Impressions']
+    nyt_data.dropna(inplace=True)
+    nyt_data.drop((nyt_data['Clicks'] > nyt_data['Impressions']).to_numpy().nonzero()[0],
+                  inplace=True)
+    compressed_nyt_data = nyt_data.groupby(by=['Age_group', 'Gender'])[['CTR','Clicks']].agg([np.mean, np.std, np.max, q25, np.median,q75, np.sum])
+    return compressed_nyt_data
+
+import pathlib
+
+
+
+def traverse(sourceFolder, collect):
+    def get_file_number(data_file):
+        return int(data_file.name[3:-4]) - 1
+    for data_file in pathlib.Path(sourceFolder).glob('∗.csv'):
+        print("Collecting data")
+        collect(summarize(data_file.absolute()), get_file_number(data_file))
+
+    
